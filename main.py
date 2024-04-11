3b4dad8d95f443c0e8e16453d140a8126ddef9f5
Recep Dogan <rdogan8891@gmail.com>
2024-04-11 15:31:00 +0200

    user class

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
@@ -6,11 +7,35 @@ app = FastAPI()
 # In-memory database
 categories = []
 
+# In-memory database
+posts = []
+
 # Pydantic model for blog data
 class Category(BaseModel):
     name: str
     slug: str
     description: str
+
+class User(BaseModel):
+    id: int
+    name: str
+    email: str
+    username: str
+    about: str
+
+class Post(BaseModel):
+    id: int
+    title: str
+    slug: str
+    description: str
+    body: str
+    image: str
+    dateCreated: datetime
+    isPuplished: bool
+    category: Category
+    author: User
+
+
 
 @app.get('/')
 def read_root():
@@ -66,3 +91,36 @@ def deletCategory(id: int):
     
     deleted_cat = categories.pop(id)
     return deleted_cat
+
+@app.get('/posts', response_model=list[Post])
+async def getAllPosts():
+    return posts
+
+@app.post('/posts', response_model=Post)
+async def createPost(newPost: Post):
+    posts.append(newPost)
+    return newPost
+
+@app.get('/posts/{id}', response_model=Post)
+def getPostById(id:int):
+    if id < 0 or id >= len(posts):
+        raise HTTPException(status_code=404, detail="Resource not found")
+    return posts[id]
+
+@app.put('/posts/{id}', response_model=Post)
+def updatePost(id: int, updatedPost: Post):
+    if id < 0 or id >= len(posts):
+        raise HTTPException(status_code=404, detail="Resource not found")
+    
+    # Update the old cat with updated cat
+    posts[id] = updatedPost
+
+    return updatedPost
+
+@app.delete('/posts/{id}', response_model=Post)
+def deletePost(id: int):
+    if id < 0 or id >= len(posts):
+        raise HTTPException(status_code=404, detail="Resource not found")
+    
+    deleted_post = posts.pop(id)
+    return deleted_post
